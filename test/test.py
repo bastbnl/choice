18a15b671813a54fc436d4a3f0173611d29e13ec
Bas ten Berge <bas+github@tenberge-ict.nl>
2015-08-10 08:40:05 +0200

    Update test.py
    Can now test on python 2.6 too

--- test/test.py
+++ test/test.py
@@ -1,20 +1,23 @@
 import choice
 
+def c():
+  print("Delete confirmed")
+
 # Get a yes or no response (default is no)
 confirm = choice.Binary('Are you sure you want to delete?', False).ask()
 if confirm:
-    deleteIt()
+    c()
 
 # Input an arbitrary value, check for correctness
 howmany = choice.Input('How many pies?', int).ask()
-print("You ordered {} pies".format(howmany))
+print("You ordered {0} pies".format(howmany))
 
 # Choose from a set of options
 entree = choice.Menu(['steak', 'potatoes', 'eggplant']).ask()
-print("You choice {}".format(entree))
+print("You choice {0}".format(entree))
 
 
-posts = ['post {}'.format(num) for num in range(15)]
+posts = ['post {0}'.format(num) for num in range(15)]
 
 resp = choice.Menu(posts, ['edit', 'delete', 'publish'], ['newpost', 'exit']).ask()
 print(resp)
